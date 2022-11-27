f124be297c0a525bb4bca3b60c186de40c4cac6b
SDAxium <sdaxium@gmail.com>
2022-11-26 23:36:11 -0500

    Proper multiplayer spawning
    Issues fixed where both players would be spawned at the same location
    
    Fixed issue where multiple audio listeners would be in a scene because of the prefabs of players being generated

--- Project-Epsilon/Assets/Scripts/Multiplayer/NetworkPlayer.cs
+++ Project-Epsilon/Assets/Scripts/Multiplayer/NetworkPlayer.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using Photon.Pun;
+using Unity.XR.CoreUtils;
 
 namespace Multiplayer
 {
@@ -7,10 +9,20 @@ namespace Multiplayer
     {
         public int reference;
 
+        private PhotonView _photonView;
         // Start is called before the first frame update
         void Start()
         {
-        
+            _photonView = GetComponent<PhotonView>();
+            reference = PhotonNetwork.CurrentRoom.PlayerCount == 1 ? 1 : 2;
+            gameObject.name = PhotonNetwork.CurrentRoom.PlayerCount == 1 ? "Player One" : "Player Two";
+
+            if (!_photonView.IsMine)
+            {
+                GetComponent<AudioListener>().enabled = false;
+                GetComponent<XROrigin>().enabled = false;
+                transform.GetChild(0).GetChild(0).GetComponent<Camera>().enabled = false;
+            }
         }
 
         // Update is called once per frame
--- Project-Epsilon/Assets/Scripts/Multiplayer/NetworkPlayerSpawner.cs
+++ Project-Epsilon/Assets/Scripts/Multiplayer/NetworkPlayerSpawner.cs
@@ -1,5 +1,8 @@
 using Photon.Pun;
+using Unity.XR.CoreUtils;
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
+using UnityEngine.XR.Interaction.Toolkit.UI;
 
 namespace Multiplayer
 {
@@ -14,10 +17,18 @@ namespace Multiplayer
             base.OnJoinedRoom();
             playerSpawn = PhotonNetwork.CurrentRoom.PlayerCount == 1 ? player1Spawn.position : player2Spawn.position;
             
-            spawnedPlayerPrefab = PhotonNetwork.Instantiate("Player",playerSpawn,transform.rotation);
-            spawnedPlayerPrefab.GetComponent<NetworkPlayer>().reference = PhotonNetwork.CurrentRoom.PlayerCount == 1 ? 1 : 2;
-            print($"Player reference is {spawnedPlayerPrefab.GetComponent<NetworkPlayer>().reference}");
+            spawnedPlayerPrefab = PhotonNetwork.Instantiate("Player",playerSpawn,Quaternion.identity);
             
+            /*
+             * There HAS to be a more elegant way of doing this but because I can't figure it out, I'll just have the
+             * player's head tracking and audio listeners turned off by default and turned on after they are spawned in
+             * Hopefully that works
+             */
+            // spawnedPlayerPrefab.GetComponent<AudioListener>().enabled = true;
+            // spawnedPlayerPrefab.GetComponent<XROrigin>().enabled = true;
+            // spawnedPlayerPrefab.transform.GetChild(0).GetChild(0).gameObject
+            //     .GetComponent<TrackedPoseDriver>().enabled = true;
+            // spawnedPlayerPrefab.transform.GetChild(0).GetChild(0).GetComponent<Camera>().enabled = true;
         }
 
         public override void OnLeftRoom()
